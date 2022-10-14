e4beb7633e05f164fe9680c5462838b183567553
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-10-14 11:22:24 -0500

    [add] logic dice section

--- _dadi/js/script.js
+++ _dadi/js/script.js
@@ -3,9 +3,18 @@
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 
-
-
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 
 const giocatore = Math.floor(Math.random() * 6) + 1;
 const computer = Math.floor(Math.random() * 6) + 1;
+
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+if(giocatore > computer) {
+  console.log('hai vinto tu');
+} else {
+  console.log('mi spiace non hai vinto');
+}
+
+console.log(giocatore);
+console.log(computer);
