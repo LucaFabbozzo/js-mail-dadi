ff52d0127f2271763d50842f937defcc6a6c96fc
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-10-15 11:05:54 -0500

    correction into mail logic

--- _mail/js/script.js
+++ _mail/js/script.js
@@ -3,35 +3,29 @@
 // stampa un messaggio appropriato sull’esito del controllo.
 
 // 1 - Chiedi all’utente la sua email,
-const emailInserita = prompt('Inserisci la tua mail');
+ const emailInserita = 'marco@hotl.com';
 
 // 2 - controlla che sia nella lista di chi può accedere,
-
 // 2.a
-const listaEmails = ['gianni@me.com', 'giulio@gmail.com', 'carmelo@gmail.com', 'marco@hotmail.com'];
-
+const listaEmails = ['gianni@me.com', 'giulio@gmail.com', 'carmelo@gmail.com', 'marco@hotmail.com', 'lucafabbozzo@gmail.com']; 
 
 let isAccountRegistrato = false;
 
-  listaEmails.push(emailInserita);
 // 2.b
  // Controllo
 
  for(let i = 0; i < listaEmails.length; i++) {
-   
-   if (emailInserita === (i + 1)) {
+  
+   if (listaEmails[i] === emailInserita) {  
      isAccountRegistrato = true;
     }
-
-    console.log(isAccountRegistrato);
-    
  }
- 
+
 //  messaggio
 
  if(isAccountRegistrato) {
   console.log('ciao bentornato')
- } else if (isAccountRegistrato) {
+ } else {
   console.log('la tua mail non esiste')
  }
 
