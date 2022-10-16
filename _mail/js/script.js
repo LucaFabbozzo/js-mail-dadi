570e7b89a7476225f6e55391ae3f25dfdc4e49b5
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-10-16 08:04:02 -0500

    [add] input into mail validator

--- _mail/js/script.js
+++ _mail/js/script.js
@@ -1,34 +1,37 @@
 // Chiedi all’utente la sua email,
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
+const input = document.querySelector('[name="email"]');
+const btnInvia = document.querySelector('.btn');
 const output = document.getElementById('output');
+const reset = document.querySelector('.reset');
 
-// 1 - Chiedi all’utente la sua email,
- const emailInserita = prompt('Scrivi qui la tua mail');
 
-// 2 - controlla che sia nella lista di chi può accedere,
-// 2.a
 const listaEmails = ['gianni@me.com', 'giulio@gmail.com', 'carmelo@gmail.com', 'marco@hotmail.com', 'lucafabbozzo@gmail.com']; 
 
 let isAccountRegistrato = false;
 
-// 2.b
- // Controllo
 
- for(let i = 0; i < listaEmails.length; i++) {
-  
-   if (listaEmails[i] === emailInserita) {  
-     isAccountRegistrato = true;
+  btnInvia.addEventListener('click', function(){
+    const emailInserita = input.value;
+    input.value = '';
+    for(let i = 0; i < listaEmails.length; i++) {
+    if (listaEmails[i] === emailInserita) {  
+      isAccountRegistrato = true;
+      }
     }
- }
 
-//  messaggio
+    if(isAccountRegistrato) {
+      output.innerText = 'Ciao bentornato! Iniziamo?';
+    } else {
+      output.innerText = 'La tua mail non è presente nel nostro database, crea il tuo account personale.';
+    }
+  })
 
- if(isAccountRegistrato) {
-   output.innerText = 'Ciao bentornato! Iniziamo?';
- } else {
-   output.innerText = 'La tua mail non è presente nel nostro database, apri il tuo account personale.';
- }
+  reset.addEventListener('click', function() {
+    output.innerText = '';
+  })
+
 
    
     
